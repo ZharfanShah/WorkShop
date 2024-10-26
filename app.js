f4012946c4db1fa1a8ef336dde00d065e820e9b6
Zharfan <zharfanshah0@gmail.com>
2024-10-26 14:54:32 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let x = 5;
+console.log(x)
+const y = 6;
+console.log(y)
+
+const word = "string" //string datatype
+const number = 1 //number datatype
+const float = 1.1111 // number datatype (IEEE 754)
+const array = ["burger","pizza","water","waffle","Coffee"]  // array data structure
+
+for(let x = 0; x <= array.length-1; x++){
+    console.log(array[x]);
+}
+
